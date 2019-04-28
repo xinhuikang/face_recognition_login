a61f03a7cf11d70249616812954d8f2f720b64b8
xinhuikang <2247139839@qq.com>
2019-04-28 20:04:52 +0800

    clean

--- codingant/employees/__init__.py
+++
@@ -1,2 +0,0 @@
-# default_app_config = 'employees.apps.EmployeesConfig'
-# default_app_config = 'employees.apps.SuitConfig'
--- codingant/face_login/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FaceLoginConfig(AppConfig):
-    name = 'face_login'
